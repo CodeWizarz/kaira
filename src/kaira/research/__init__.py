e4c57d0c2fad9626baa31fe5a3ec7d86dd67517e
bobby <dveloperbobby@gmail.com>
2026-02-04 20:13:20 +0530

    Add mispricing detector with risk filters

--- src/kaira/research/__init__.py
+++ src/kaira/research/__init__.py
@@ -8,6 +8,10 @@ from kaira.research.regime_classifier import (
     regime_stability_bootstrap,
     summarize_regimes,
 )
+from kaira.research.mispricing_detector import (
+    MispricingDetectorConfig,
+    detect_mispricing_candidates,
+)
 
 __all__ = [
     "MarketRegimeClassifier",
@@ -16,4 +20,6 @@ __all__ = [
     "compute_regime_features",
     "regime_stability_bootstrap",
     "summarize_regimes",
+    "MispricingDetectorConfig",
+    "detect_mispricing_candidates",
 ]
